be6ad3b3b88fb8e7ba4f8e381f2a8e9a16adf0d9
persiklover <dannniska@gmail.com>
2020-12-26 22:03:49 +0300

    section1

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,22 @@ $('a[href^="#"]').click(function (e) {
   e.preventDefault();
   scrollToElement($(this).attr('href'));
 }); // Your code goes here...
+
+$('.gallery').addClass("owl-carousel").owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  items: 4,
+  margin: 10,
+  // onInitialized: onPortfolioSliderUpdate,
+  responsive: {
+    0: {
+      mouseDrag: true,
+      touchDrag: true
+    },
+    768: {
+      mouseDrag: false,
+      touchDrag: false
+    }
+  }
+});
