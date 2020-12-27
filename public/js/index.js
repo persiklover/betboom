b845f92d8db8d5696c36ff9de2220e015b09e22a
persiklover <dannniska@gmail.com>
2020-12-27 15:40:37 +0300

    section5

--- public/js/index.js
+++ public/js/index.js
@@ -13,21 +13,39 @@ $('a[href^="#"]').click(function (e) {
   scrollToElement($(this).attr('href'));
 }); // Your code goes here...
 
-$('.gallery').addClass("owl-carousel").owlCarousel({
+var $gallery = $('.gallery');
+$gallery.children().each(function (index) {
+  $(this).attr('data-position', index); // NB: .attr() instead of .data()
+});
+$gallery.addClass("owl-carousel").owlCarousel({
   loop: true,
   nav: false,
   dots: true,
   items: 4,
   margin: 10,
-  // onInitialized: onPortfolioSliderUpdate,
   responsive: {
     0: {
+      items: 1,
       mouseDrag: true,
       touchDrag: true
     },
     768: {
+      items: 4,
       mouseDrag: false,
       touchDrag: false
     }
+  }
+});
+var prevPosition = 0;
+$(document).on('click', '.owl-item > *', function () {
+  // BUG: решить, что делать, когда position < prevPosition
+  var speed = 300;
+  var position = $(this).data('position');
+  var len = position - prevPosition;
+  console.log(prevPosition, position, len);
+  prevPosition = position;
+
+  for (var i = 0; i < len; i++) {
+    $gallery.trigger('next.owl.carousel', [speed]);
   }
 });
--- src/js/index.js
+++ src/js/index.js
@@ -13,23 +13,52 @@ $('a[href^="#"]').click(function(e) {
 
 // Your code goes here...
 
-$('.gallery')
-  .addClass("owl-carousel")
+const $gallery = $('.gallery');
+
+$gallery.children().each(function (index) {
+  $(this).attr('data-position', index); // NB: .attr() instead of .data()
+});
+
+
+$gallery.addClass("owl-carousel")
   .owlCarousel({
     loop: true,
     nav:  false,
     dots: true,
     items:  4,
     margin: 10,
-    // onInitialized: onPortfolioSliderUpdate,
+
     responsive: {
       0: {
+        items: 1,
         mouseDrag: true,
         touchDrag: true,
       },
       768: {
+        items: 4,
         mouseDrag: false,
         touchDrag: false,
       }
     }
-  })
+  });
+
+let prevPosition = 0;
+$(document).on('click', '.owl-item > *', function () {
+
+  // BUG: решить, что делать, когда position < prevPosition
+  
+  const speed = 300;
+  const position = $(this).data('position');
+  const len = position - prevPosition;
+  console.log(
+    prevPosition,
+    position,
+    len
+  );
+
+  prevPosition = position;
+
+  for (let i = 0; i < len; i++) {
+    $gallery.trigger('next.owl.carousel', [speed]);
+  }
+});
