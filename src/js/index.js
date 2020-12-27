2ae3362b472779645718d0fe3b1aa16643fc907a
persiklover <dannniska@gmail.com>
2020-12-27 21:48:06 +0300

    section3

--- src/js/index.js
+++ src/js/index.js
@@ -6,26 +6,89 @@ function scrollToElement(selector, callback) {
   }, 500, callback);
 }
 
-$('a[href^="#"]').click(function(e) {
+$('a[href^="#"]').click(function (e) {
   e.preventDefault();
-  scrollToElement( $(this).attr('href') );
+  scrollToElement($(this).attr('href'));
 });
 
-// Your code goes here...
+// Подать заявку
+const $formSlider = $(".form-steps");
+
+function onFormSubmit() {
+  console.log("Submit!");
+  $formSlider.trigger("next.owl.carousel");
+}
+
+function onFromSliderChanged(event) {
+  var current = event.item.index + 1;
+  var total = event.item.count - 3;
+
+  const $step = $formSlider.find(`.form-step`)[current - 1];
+  const title = $step.dataset.title;
+
+  $(".js-form-current").text(current);
+  $(".js-form-total").text(total);
+
+  $formSlider.find(".owl-dots").attr("hidden", current > 2);
+
+  $(".form__title-prefix").attr("hidden", current > 2);
+  if (title) {
+    $(".js-form-title").text(title);
+  }
+}
+
+$formSlider
+  .addClass("owl-carousel")
+  .owlCarousel({
+    loop: false,
+    nav: false,
+    dots: true,
+    items: 1,
+    margin: 1,
+    mouseDrag: false,
+    touchDrag: false,
+    onInitialized: onFromSliderChanged,
+    responsive: {
+      0: {
+        autoHeight: true,
+      },
+      768: {
+        autoHeight: false,
+      }
+    }
+  })
+  .on("changed.owl.carousel", onFromSliderChanged);
+
+$(".js-form-next").click(function (e) {
+  e.preventDefault();
+  $formSlider.trigger("next.owl.carousel");
+});
+
+$(".js-form-prev").click(function (e) {
+  e.preventDefault();
+  $formSlider.trigger("prev.owl.carousel");
+});
+
+$(".form").submit(function (e) {
+  e.preventDefault();
+  onFormSubmit();
+});
+
+// Галерея
 
 const $gallery = $('.gallery');
 
 $gallery.children().each(function (index) {
-  $(this).attr('data-position', index); // NB: .attr() instead of .data()
+  $(this).attr('data-position', index);
 });
 
-
-$gallery.addClass("owl-carousel")
+$gallery
+  .addClass("owl-carousel")
   .owlCarousel({
     loop: true,
-    nav:  false,
+    nav: false,
     dots: true,
-    items:  4,
+    items: 4,
     margin: 10,
 
     responsive: {
@@ -43,18 +106,13 @@ $gallery.addClass("owl-carousel")
   });
 
 let prevPosition = 0;
-$(document).on('click', '.owl-item > *', function () {
+$(document).on('click', '.gallery .owl-item > *', function () {
 
   // BUG: решить, что делать, когда position < prevPosition
-  
+
   const speed = 300;
   const position = $(this).data('position');
   const len = position - prevPosition;
-  console.log(
-    prevPosition,
-    position,
-    len
-  );
 
   prevPosition = position;
 
